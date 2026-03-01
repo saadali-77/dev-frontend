dcb7030fcc4a94dcb741de0571c7532a62b03cf8
saadali-77 <saadali6991550@gmail.com>
2026-03-01 13:50:59 +0500

    Export BASE_URL constant for backend URL

--- src/appStore/constant.js
+++ src/appStore/constant.js
@@ -1,4 +1,4 @@
 //export const BASE_URL= "http://localhost:3000"
 //const BASE_URL = import.meta.env.VITE_BASE_URL;
-const BASE_URL = "https://dev-backend-kohl.vercel.app" || "http://localhost:3000";
+ export const BASE_URL = "https://dev-backend-kohl.vercel.app" 
 
