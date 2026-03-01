16e2001fd9347b7b4c4b221e01dc2a51017e1156
saadali-77 <saadali6991550@gmail.com>
2026-03-01 15:15:12 +0500

    Update BASE_URL to new development backend

--- src/appStore/constant.js
+++ src/appStore/constant.js
@@ -1,4 +1,4 @@
 //export const BASE_URL= "http://localhost:3000"
 //const BASE_URL = import.meta.env.VITE_BASE_URL;
- export const BASE_URL = "https://dev-backend-kohl.vercel.app" 
+// export const BASE_URL = "https://dev-backend-kohl.vercel.app" 
 export const BASE_URL =    "https://dev-backend-3vfj.vercel.app"
