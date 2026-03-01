aa6b43d1f33455771769807f381eb4c01659500c
saadali-77 <saadali6991550@gmail.com>
2026-03-01 15:51:42 +0500

    Update login API endpoint to /api/auth/login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -21,7 +21,7 @@ const Navigate= useNavigate()
 
     try {
       const res = await axios.post(
-          BASE_URL + "/api/login",
+          BASE_URL + "/api/auth/login",
         {
           emailId: form.emailId,
           password: form.password,
