e48835e4d2782afe3504aef4f6202d6e05859e77
saadali-77 <saadali6991550@gmail.com>
2026-07-19 19:54:34 +0500

    Update constant.js

--- src/appStore/constant.js
+++ src/appStore/constant.js
@@ -1,5 +1,5 @@
 //export const BASE_URL= "http://localhost:3000"
 //const BASE_URL = import.meta.env.VITE_BASE_URL;
 // export const BASE_URL = "https://dev-backend-kohl.vercel.app" 
-export const BASE_URL =    "https://dev-backend-roke.vercel.app "
+export const BASE_URL =    "https://dev-backend-roke.vercel.app"
 
